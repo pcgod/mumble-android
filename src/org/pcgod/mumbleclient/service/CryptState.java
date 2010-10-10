42a684832fdd3967d6191640bd5b317e0b7da9de
Benjamin Jemlich <benjamin@jemlich.de>
2010-10-11 00:30:04 +0200

    Add CryptState implementation

---
+++ src/org/pcgod/mumbleclient/service/CryptState.java
@@ -0,0 +1,326 @@
+package org.pcgod.mumbleclient.service;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+public class CryptState {
+	private static final int AES_BLOCK_SIZE = 16;
+
+	private static void S2(final byte[] block) {
+		final int carry = (block[0] >> 7) & 0x1;
+		for (int i = 0; i < AES_BLOCK_SIZE - 1; i++) {
+			block[i] = (byte) ((block[i] << 1) | ((block[i + 1] >> 7) & 0x1));
+		}
+		block[AES_BLOCK_SIZE - 1] = (byte) ((block[AES_BLOCK_SIZE - 1] << 1) ^ (carry * 0x87));
+	}
+
+	private static void S3(final byte[] block) {
+		final int carry = (block[0] >> 7) & 0x1;
+		for (int i = 0; i < AES_BLOCK_SIZE - 1; i++) {
+			block[i] ^= (block[i] << 1) | ((block[i + 1] >> 7) & 0x1);
+		}
+		block[AES_BLOCK_SIZE - 1] ^= ((block[AES_BLOCK_SIZE - 1] << 1) ^ (carry * 0x87));
+	}
+
+	private static void XOR(final byte[] dst, final byte[] a, final byte[] b) {
+		for (int i = 0; i < AES_BLOCK_SIZE; i++) {
+			dst[i] = (byte) (a[i] ^ b[i]);
+		}
+	}
+
+	private static void ZERO(final byte[] block) {
+		Arrays.fill(block, (byte) 0);
+	}
+
+	private final byte[] decryptHistory = new byte[256];
+
+	private Cipher encryptCipher;
+	private Cipher decryptCipher;
+	private byte[] encryptIv;
+	private byte[] decryptIv;
+	private boolean initialized = false;
+	private int good;
+	private int late;
+	private int lost;
+
+	public byte[] Decrypt(final byte[] source) {
+		if (source.length < 4) {
+			return null;
+		}
+
+		final int plain_length = source.length - 4;
+		final byte[] dst = new byte[plain_length];
+
+		final byte[] saveiv = new byte[AES_BLOCK_SIZE];
+		final byte ivbyte = source[0];
+		boolean restore = false;
+		final byte[] tag = new byte[AES_BLOCK_SIZE];
+
+		int lost = 0;
+		int late = 0;
+
+		System.arraycopy(decryptIv, 0, saveiv, 0, AES_BLOCK_SIZE);
+
+		if (((decryptIv[0] + 1) & 0xFF) == ivbyte) {
+			// In order as expected.
+			if (ivbyte > decryptIv[0]) {
+				decryptIv[0] = ivbyte;
+			} else if (ivbyte < decryptIv[0]) {
+				decryptIv[0] = ivbyte;
+				for (int i = 1; i < AES_BLOCK_SIZE; i++) {
+					if ((++decryptIv[i]) != 0) {
+						break;
+					}
+				}
+			} else {
+				return null;
+			}
+		} else {
+			// This is either out of order or a repeat.
+			int diff = ivbyte - decryptIv[0];
+			if (diff > 128) {
+				diff = diff - 256;
+			} else if (diff < -128) {
+				diff = diff + 256;
+			}
+
+			if ((ivbyte < decryptIv[0]) && (diff > -30) && (diff < 0)) {
+				// Late packet, but no wraparound.
+				late = 1;
+				lost = -1;
+				decryptIv[0] = ivbyte;
+				restore = true;
+			} else if ((ivbyte > decryptIv[0]) && (diff > -30) && (diff < 0)) {
+				// Last was 0x02, here comes 0xff from last round
+				late = 1;
+				lost = -1;
+				decryptIv[0] = ivbyte;
+				for (int i = 1; i < AES_BLOCK_SIZE; i++) {
+					if ((decryptIv[i]--) != 0) {
+						break;
+					}
+				}
+				restore = true;
+			} else if ((ivbyte > decryptIv[0]) && (diff > 0)) {
+				// Lost a few packets, but beyond that we're good.
+				lost = ivbyte - decryptIv[0] - 1;
+				decryptIv[0] = ivbyte;
+			} else if ((ivbyte < decryptIv[0]) && (diff > 0)) {
+				// Lost a few packets, and wrapped around
+				lost = 256 - decryptIv[0] + ivbyte - 1;
+				decryptIv[0] = ivbyte;
+				for (int i = 1; i < AES_BLOCK_SIZE; i++) {
+					if ((++decryptIv[i]) != 0) {
+						break;
+					}
+				}
+			} else {
+				return null;
+			}
+
+			if (decryptHistory[decryptIv[0]] == decryptIv[1]) {
+				System.arraycopy(saveiv, 0, decryptIv, 0, AES_BLOCK_SIZE);
+				return null;
+			}
+		}
+
+		final byte[] newsrc = new byte[plain_length];
+		System.arraycopy(source, 4, newsrc, 0, plain_length);
+		try {
+			OcbDecrypt(newsrc, dst, decryptIv, tag);
+		} catch (final IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (final BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (tag[0] != source[1] || tag[1] != source[2] || tag[2] != source[3]) {
+			System.arraycopy(saveiv, 0, decryptIv, 0, AES_BLOCK_SIZE);
+			return null;
+		}
+		decryptHistory[decryptIv[0]] = decryptIv[1];
+
+		if (restore) {
+			System.arraycopy(saveiv, 0, decryptIv, 0, AES_BLOCK_SIZE);
+		}
+
+		good++;
+		this.late += late;
+		this.lost += lost;
+
+		return dst;
+	}
+
+	public byte[] Encrypt(final byte[] source) {
+		final byte[] tag = new byte[AES_BLOCK_SIZE];
+
+		// First, increase our IV.
+		for (int i = 0; i < AES_BLOCK_SIZE; i++) {
+			if ((++encryptIv[i]) != 0) {
+				break;
+			}
+		}
+
+		final byte[] dst = new byte[source.length + 4];
+		try {
+			OcbEncrypt(source, dst, encryptIv, tag);
+		} catch (final IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (final BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.arraycopy(dst, 0, dst, 4, source.length);
+		dst[0] = encryptIv[0];
+		dst[1] = tag[0];
+		dst[2] = tag[1];
+		dst[3] = tag[2];
+
+		return dst;
+	}
+
+	public boolean isInitialized() {
+		return initialized;
+	}
+
+	public void SetKeys(final byte[] rkey, final byte[] eiv, final byte[] div) {
+		initialized = false;
+		try {
+			encryptCipher = Cipher.getInstance("AES/ECB/NoPadding");
+			decryptCipher = Cipher.getInstance("AES/ECB/NoPadding");
+		} catch (final NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return;
+		} catch (final NoSuchPaddingException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		final SecretKeySpec cryptKey = new SecretKeySpec(rkey, "AES");
+		encryptIv = Arrays.copyOf(eiv, eiv.length);
+		decryptIv = Arrays.copyOf(div, div.length);
+
+		try {
+			encryptCipher.init(Cipher.ENCRYPT_MODE, cryptKey);
+			decryptCipher.init(Cipher.DECRYPT_MODE, cryptKey);
+		} catch (final InvalidKeyException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		initialized = true;
+	}
+
+	private void OcbDecrypt(
+		final byte[] encrypted,
+		final byte[] plain,
+		final byte[] nonce,
+		final byte[] tag) throws IllegalBlockSizeException, BadPaddingException {
+		final byte[] checksum = new byte[AES_BLOCK_SIZE];
+		byte[] tmp = new byte[AES_BLOCK_SIZE];
+
+		final byte[] delta = encryptCipher.doFinal(nonce);
+
+		int offset = 0;
+		int len = encrypted.length;
+		while (len > AES_BLOCK_SIZE) {
+			final byte[] buffer = new byte[AES_BLOCK_SIZE];
+			S2(delta);
+			System.arraycopy(encrypted, offset, buffer, 0, AES_BLOCK_SIZE);
+
+			XOR(tmp, delta, buffer);
+			tmp = decryptCipher.doFinal(tmp);
+
+			XOR(buffer, delta, tmp);
+			System.arraycopy(buffer, 0, plain, offset, AES_BLOCK_SIZE);
+
+			XOR(checksum, checksum, buffer);
+			len -= AES_BLOCK_SIZE;
+			offset += AES_BLOCK_SIZE;
+		}
+
+		S2(delta);
+		ZERO(tmp);
+
+		final long num = len * 8;
+		tmp[AES_BLOCK_SIZE - 2] = (byte) ((num >> 8) & 0xFF);
+		tmp[AES_BLOCK_SIZE - 1] = (byte) (num & 0xFF);
+		XOR(tmp, tmp, delta);
+
+		final byte[] pad = encryptCipher.doFinal(tmp);
+		ZERO(tmp);
+		System.arraycopy(encrypted, offset, tmp, 0, len);
+
+		XOR(tmp, tmp, pad);
+		XOR(checksum, checksum, tmp);
+
+		System.arraycopy(tmp, 0, plain, offset, len);
+
+		S3(delta);
+		XOR(tmp, delta, checksum);
+
+		final byte[] tmp_tag = encryptCipher.doFinal(tmp);
+		System.arraycopy(tmp_tag, 0, tag, 0, AES_BLOCK_SIZE);
+	}
+
+	private void OcbEncrypt(
+		final byte[] plain,
+		final byte[] encrypted,
+		final byte[] nonce,
+		final byte[] tag) throws IllegalBlockSizeException, BadPaddingException {
+		final byte[] checksum = new byte[AES_BLOCK_SIZE];
+		byte[] tmp = new byte[AES_BLOCK_SIZE];
+
+		final byte[] delta = encryptCipher.doFinal(nonce);
+
+		int offset = 0;
+		int len = plain.length;
+		while (len > AES_BLOCK_SIZE) {
+			final byte[] buffer = new byte[AES_BLOCK_SIZE];
+			S2(delta);
+			System.arraycopy(plain, offset, buffer, 0, AES_BLOCK_SIZE);
+			XOR(checksum, checksum, buffer);
+			XOR(tmp, delta, buffer);
+
+			tmp = encryptCipher.doFinal(tmp);
+
+			XOR(buffer, delta, tmp);
+			System.arraycopy(buffer, 0, encrypted, offset, AES_BLOCK_SIZE);
+			len -= AES_BLOCK_SIZE;
+			offset += AES_BLOCK_SIZE;
+		}
+
+		S2(delta);
+		ZERO(tmp);
+		final long num = len * 8;
+		tmp[AES_BLOCK_SIZE - 2] = (byte) ((num >> 8) & 0xFF);
+		tmp[AES_BLOCK_SIZE - 1] = (byte) (num & 0xFF);
+		XOR(tmp, tmp, delta);
+
+		final byte[] pad = encryptCipher.doFinal(tmp);
+
+		System.arraycopy(plain, offset, tmp, 0, len);
+		System.arraycopy(pad, len, tmp, len, AES_BLOCK_SIZE - len);
+
+		XOR(checksum, checksum, tmp);
+		XOR(tmp, pad, tmp);
+		System.arraycopy(tmp, 0, encrypted, offset, len);
+
+		S3(delta);
+		XOR(tmp, delta, checksum);
+
+		final byte[] tmp_tag = encryptCipher.doFinal(tmp);
+		System.arraycopy(tmp_tag, 0, tag, 0, AES_BLOCK_SIZE);
+	}
+}
