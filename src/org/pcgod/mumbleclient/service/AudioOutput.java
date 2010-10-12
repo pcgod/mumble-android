69913c1a654407f8cd985e39ff66f34767ca885e
Mikko Rantanen <jubjub@jubjubnest.net>
2010-10-13 00:12:37 +0300

    Reduced audio output latency when starting playback from standby.
    - Delayed AudioTrack.play until the track has some buffered input which prevents the track from trying to obtain buffer while it is empty.
    - Pausing playback when it has been silent for a while (2000ms currently) so it can be buffered when it starts again.

--- src/org/pcgod/mumbleclient/service/AudioOutput.java
+++ src/org/pcgod/mumbleclient/service/AudioOutput.java
@@ -7,12 +7,14 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.pcgod.mumbleclient.Globals;
 import org.pcgod.mumbleclient.service.AudioUser.PacketReadyHandler;
 import org.pcgod.mumbleclient.service.model.User;
 
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
+import android.util.Log;
 
 /**
  * Audio output thread.
@@ -36,28 +38,37 @@ class AudioOutput implements Runnable {
 		}
 	};
 
+	private final static int standbyTreshold = 2000;
+
 	private boolean shouldRun;
 	private final AudioTrack at;
 	private final int bufferSize;
+	private final int minBufferSize;
 
 	final Map<User, AudioUser> userPackets = new HashMap<User, AudioUser>();
 	private final Map<User, AudioUser> users = new HashMap<User, AudioUser>();
+
+	/**
+	 * Buffer used to hold temporary float values while mixing multiple
+	 * inputs. Only for use in the audio thread.
+	 */
+	final float[] tempMix = new float[MumbleConnection.FRAME_SIZE];
 
 	private final AudioOutputHost host;
 
 	public AudioOutput(final AudioOutputHost host) {
 		this.host = host;
 
-		int minBufferSize = AudioTrack.getMinBufferSize(
+		minBufferSize = AudioTrack.getMinBufferSize(
 			MumbleConnection.SAMPLE_RATE,
 			AudioFormat.CHANNEL_CONFIGURATION_MONO,
 			AudioFormat.ENCODING_PCM_16BIT);
 
 		// Double the buffer size to reduce stuttering.
-		minBufferSize *= 2;
+		final int desiredBufferSize = minBufferSize * 2;
 
 		// Resolve the minimum frame count that fills the minBuffer requirement.
-		final int frameCount = (int) Math.ceil((double) minBufferSize /
+		final int frameCount = (int) Math.ceil((double) desiredBufferSize /
 											   MumbleConnection.FRAME_SIZE);
 
 		bufferSize = frameCount * MumbleConnection.FRAME_SIZE;
@@ -114,58 +125,121 @@ class AudioOutput implements Runnable {
 	}
 
 	private void audioLoop() throws InterruptedException {
-		final float[] out = new float[MumbleConnection.FRAME_SIZE];
-		final short[] clipOut = new short[MumbleConnection.FRAME_SIZE];
+		final short[] out = new short[MumbleConnection.FRAME_SIZE];
 		final List<AudioUser> mix = new LinkedList<AudioUser>();
 
-		at.play();
+		int buffered = 0;
+		boolean playing = false;
+
 		while (shouldRun) {
 			mix.clear();
 
-			synchronized (userPackets) {
-				final Iterator<AudioUser> i = userPackets.values().iterator();
-				while (i.hasNext()) {
-					final AudioUser user = i.next();
-					if (user.hasFrame()) {
-						mix.add(user);
-					} else {
-						i.remove();
-						host.setTalkState(
-							user.getUser(),
-							AudioOutputHost.STATE_PASSIVE);
-					}
-				}
-			}
+			// Get mix frames from the AudioUsers
+			fillMixFrames(mix);
 
 			// If there is output, play it now.
 			if (mix.size() > 0) {
-				// Reset mix buffer.
-				Arrays.fill(out, 0);
+				// Mix all the frames into one array.
+				mix(out, mix);
 
-				// Sum the buffers.
-				for (final AudioUser user : mix) {
-					for (int i = 0; i < out.length; i++) {
-						out[i] += user.lastFrame[i];
+				at.write(out, 0, MumbleConnection.FRAME_SIZE);
+
+				// Make sure we are playing when there are enough samples
+				// buffered.
+				if (!playing) {
+					buffered += out.length;
+
+					if (buffered >= minBufferSize) {
+						at.play();
+						playing = true;
+						buffered = 0;
+
+						Log.i(
+							Globals.LOG_TAG,
+							"AudioOutput: Enough data buffered. Starting audio.");
 					}
 				}
 
-				// Clip buffer for real output.
-				for (int i = 0; i < MumbleConnection.FRAME_SIZE; i++) {
-					clipOut[i] = (short) (Short.MAX_VALUE * (out[i] < -1.0f ? -1.0f
-						: (out[i] > 1.0f ? 1.0f : out[i])));
-				}
-
-				at.write(clipOut, 0, MumbleConnection.FRAME_SIZE);
+				// Continue with playback since we know that there is at least
+				// one AudioUser in userPackets that wasn't removed as it had
+				// frames for mixing.
+				continue;
 			}
 
 			// Wait for more input.
-			synchronized (userPackets) {
+			playing &= !pauseForInput();
+			if (!playing && buffered > 0) {
+				Log.w(
+					Globals.LOG_TAG,
+					"AudioOutput: Stopped playing while buffered data present.");
+			}
+		}
+		at.flush();
+		at.stop();
+	}
+
+	private void fillMixFrames(final List<AudioUser> mix) {
+		synchronized (userPackets) {
+			final Iterator<AudioUser> i = userPackets.values().iterator();
+			while (i.hasNext()) {
+				final AudioUser user = i.next();
+				if (user.hasFrame()) {
+					mix.add(user);
+				} else {
+					i.remove();
+					host.setTalkState(
+						user.getUser(),
+						AudioOutputHost.STATE_PASSIVE);
+				}
+			}
+		}
+	}
+
+	private void mix(final short[] clipOut, final List<AudioUser> mix) {
+		// Reset mix buffer.
+		Arrays.fill(tempMix, 0);
+
+		// Sum the buffers.
+		for (final AudioUser user : mix) {
+			for (int i = 0; i < tempMix.length; i++) {
+				tempMix[i] += user.lastFrame[i];
+			}
+		}
+
+		// Clip buffer for real output.
+		for (int i = 0; i < MumbleConnection.FRAME_SIZE; i++) {
+			clipOut[i] = (short) (Short.MAX_VALUE * (tempMix[i] < -1.0f ? -1.0f
+				: (tempMix[i] > 1.0f ? 1.0f : tempMix[i])));
+		}
+	}
+
+	private boolean pauseForInput() throws InterruptedException {
+		long silentTime;
+		boolean paused = false;
+		synchronized (userPackets) {
+			silentTime = System.currentTimeMillis();
+
+			// Wait with the audio on
+			while (shouldRun && userPackets.isEmpty() &&
+				   (silentTime + standbyTreshold) > System.currentTimeMillis()) {
+
+				userPackets.wait((silentTime + standbyTreshold) -
+								 System.currentTimeMillis() + 1);
+			}
+
+			// If conditions are still not filled, pause audio and wait more.
+			if (shouldRun && userPackets.isEmpty()) {
+				at.pause();
+				paused = true;
+				Log.i(
+					Globals.LOG_TAG,
+					"AudioOutput: Standby timeout reached. Audio paused.");
+
 				while (shouldRun && userPackets.isEmpty()) {
 					userPackets.wait();
 				}
 			}
 		}
-		at.flush();
-		at.stop();
+		return paused;
 	}
 }
